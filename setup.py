bdcd6d6e2f22cff37a2ccbd49dbe096de32bcbdf
Nik Krumm <nkrumm@gmail.com>
2013-05-16 15:45:29 -0700

    move/rename of directories

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from setuptools import setup
 
-setup(name='conifer-tools/',
+setup(name='conifertools',
       version='0.1',
       description='A collection of helpful tools for use with CoNIFER',
       url='http://github.com/nkrumm/conifer-tools',
+      download_url = 'https://github.com/nkrumm/conifer-tools/tarball/0.1',
       author='Niklas Krumm',
       author_email='nkrumm@gmail.com',
       license='',
-      packages=['conifer-tools/'],
+      packages=['conifertools'],
       install_requires=[
           'numpy',
           'tables',
