76db7e7c5717ec0963ec26ab6df57cf04f754cd7
Nik Krumm <nkrumm@gmail.com>
2013-06-03 12:07:30 -0700

    simple driver script for QC_SampleSD_plot function

---
+++ scripts/QC_01_sample_sd.py
@@ -0,0 +1,31 @@
+from conifertools import ConiferPipeline
+import argparse
+import numpy as np
+
+if __name__ == "__main__":
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--infile", "-i", action="store", required=True)
+parser.add_argument("--outfile", "-o", action="store", required=True)
+parser.add_argument("--plotfile", action="store", required=False, default=None)
+parser.add_argument("--verbose", action="store_true", required=False, default=False)
+args = parser.parse_args(["--infile","/net/eichler/vol8/home/nkrumm/EXOMES/ESP2000/SVD/ESP.BROAD.SVD40.QC.SampleQC.hdf5", "--outfile","test.sd.txt", "--plotfile","test.png", "--verbose"])
+
+p = ConiferPipeline(args.infile)
+#n_probes = sum([len(p.getProbesByChrom(x)) for x in range(1,24)])
+out_sd_values = []
+with open(args.outfile, 'w') as out_file:
+    for sampleID in p.samples:
+        sample_data = []
+        for c in xrange(1,24):
+            sample_data.extend(p.getConiferData(sampleID, c).rpkm)
+        sample_sd = np.std(np.array(sample_data))
+        out_sd_values.append(sample_sd)
+        if args.verbose:
+            print sampleID, sample_sd
+        out_file.write("%s\t%f\n" % (sampleID, sample_sd))
+
+    if args.plotfile:
+        from conifertools.plotting import QC_SampleSD_Plot
+        kwargs = {"bins": 20, "color":'r'}
+        QC_SampleSD_Plot(out_sd_values, title='SD values', outfile=args.plotfile, logscale=True, **kwargs)
